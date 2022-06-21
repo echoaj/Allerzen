cc8a8f32a6dc18d68bcd27e10e277767aff189ae
echoaj <aljoslin13@yahoo.com>
2022-06-21 12:52:13 -0700

    enzo thomas lesson

--- src/survey/views.py
+++ src/survey/views.py
@@ -5,4 +5,13 @@ from django.http.response import HttpResponse, JsonResponse
 # Create your views here.
 def home_view(request):
     print("User on website")
-    return JsonResponse({"message": "hi"})
+    if request.method == "GET":
+        return render(request, 'home.html', {'range': range(10)})
+    elif request.method == "POST":
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        age = request.POST.get('age')
+        print(request.POST)
+        print(first_name, last_name)
+        print(age)
+        return render(request, 'home.html', {"success": True})
